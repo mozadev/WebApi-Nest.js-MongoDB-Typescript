07a9c219b53d30a6e89ca6c11fe764c9b7c088cf
ceosmore <ceosmore@gmail.com>
2024-05-19 21:19:58 -0500

    chore: Update npm dependencies and add MongooseModule to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { join } from 'path'; // en node , usually goin at start of file
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { Mongoose } from 'mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
 
 // the module decorator is used to define a module
 // module always goint inside of import array
@@ -10,6 +12,9 @@ import { PokemonModule } from './pokemon/pokemon.module';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+
     PokemonModule,
   ],
 })
