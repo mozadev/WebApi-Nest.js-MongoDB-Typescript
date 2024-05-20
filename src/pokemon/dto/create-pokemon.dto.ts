2ba7cdd77233cf55a45fc5360ff3c3dde2222c50
ceosmore <ceosmore@gmail.com>
2024-05-20 18:13:10 -0500

    chore: Add class-transformer and class-validator dependencies for input validation in CreatePokemonDto

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,19 @@
-export class CreatePokemonDto {}
+import {
+  IsInt,
+  IsPositive,
+  IsString,
+  MinLength,
+  isString,
+} from 'class-validator';
+
+export class CreatePokemonDto {
+  //isInt, isPositive, min1
+  @IsInt()
+  @IsPositive()
+  @MinLength(1)
+  no: number;
+  //isString, min1
+  @IsString()
+  @MinLength(1)
+  name: string;
+}
