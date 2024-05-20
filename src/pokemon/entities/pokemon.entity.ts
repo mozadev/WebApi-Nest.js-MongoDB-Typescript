4d2e18b5a5644092c94653f76512e6eb0bd7605d
ceosmore <ceosmore@gmail.com>
2024-05-20 17:28:52 -0500

    chore: Add Pokemon entity and schema with Mongoose integration

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,11 @@
-export class Pokemon {}
+import { Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Pokemon extends Document {
+  //id: string; mongo just give me the id
+  name: string;
+  no: number;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
