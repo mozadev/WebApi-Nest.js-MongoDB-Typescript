2d32d648343606dd86c8ddffd776f7aacf5718b1
ceosmore <ceosmore@gmail.com>
2024-05-22 22:15:42 -0500

    chore: Update ParseMongoIdPipe to accept only string values

--- src/common/pipes/parse-mongo-id/parse-mongo-id.pipe.ts
+++ src/common/pipes/parse-mongo-id/parse-mongo-id.pipe.ts
@@ -2,7 +2,8 @@ import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
 
 @Injectable()
 export class ParseMongoIdPipe implements PipeTransform {
-  transform(value: any, metadata: ArgumentMetadata) {
+  transform(value: string, metadata: ArgumentMetadata) {
+    console.log({ value, metadata });
     return value;
   }
 }
