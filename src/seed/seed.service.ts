6dbba08d96ac859363ee6fee9ba42cafa7fa281d
ceosmore <ceosmore@gmail.com>
2024-05-23 16:39:07 -0500

    feat: Add axios dependency and use it to fetch data from external API

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 
 @Injectable()
 export class SeedService {
+  //this is dependency de axios en us project
+  private readonly axios: AxiosInstance;
+
   executedSeed() {
     axios.get('https://pokeapi.co/api/v2/pokemon?limit=650&offset=0');
     return 'This action returns all seed';
