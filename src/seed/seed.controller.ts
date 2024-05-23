9acaef25ef50ef495422906d44e61acf4dbab678
ceosmore <ceosmore@gmail.com>
2024-05-23 13:31:05 -0500

    Remove Dto and Entities from  the Seed module since it doesn't use custom parameters, nor will it be saved in the database

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,16 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { SeedService } from './seed.service';
-import { CreateSeedDto } from './dto/create-seed.dto';
-import { UpdateSeedDto } from './dto/update-seed.dto';
 
 @Controller('seed')
 export class SeedController {
   constructor(private readonly seedService: SeedService) {}
-
-  @Post()
-  create(@Body() createSeedDto: CreateSeedDto) {
-    return this.seedService.create(createSeedDto);
-  }
 
   @Get()
   findAll() {
