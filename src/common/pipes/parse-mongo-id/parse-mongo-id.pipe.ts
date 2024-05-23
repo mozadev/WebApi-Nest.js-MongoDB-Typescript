3502d5fe312eb2524fbedc896c2ab80cd893cbb0
ceosmore <ceosmore@gmail.com>
2024-05-22 22:34:35 -0500

    chore: Update ParseMongoIdPipe to validate and transform MongoDB IDs

--- src/common/pipes/parse-mongo-id/parse-mongo-id.pipe.ts
+++ src/common/pipes/parse-mongo-id/parse-mongo-id.pipe.ts
@@ -1,9 +1,20 @@
-import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+import { isValidObjectId } from 'mongoose';
 
 @Injectable()
 export class ParseMongoIdPipe implements PipeTransform {
+  // the pipes transform phisically changes the value
   transform(value: string, metadata: ArgumentMetadata) {
-    console.log({ value, metadata });
-    return value.toLocaleUpperCase();
+    // console.log({ value, metadata });
+    if (!isValidObjectId(value)) {
+      throw new BadRequestException(`${value} is not a valid mongoId`);
+    }
+    return value.toUpperCase();
   }
 }
+// this sure that all must be mongoid when coming to the controller, comin from the client url
