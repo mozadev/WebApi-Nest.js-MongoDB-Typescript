6ab6663305634be73b9d6c77bfa67aafd6ada518
ceosmore <ceosmore@gmail.com>
2024-05-24 17:15:22 -0500

    Add export [Mongose.service, PokemonService ] in pokemon.module.ts ans import PokemonModule in seed.module.ts, Add constructor on seed.service, inside add InjectModel Pokemon

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -15,5 +15,6 @@ import { MongooseModule } from '@nestjs/mongoose';
       },
     ]),
   ],
+  exports: [PokemonService, MongooseModule],
 })
 export class PokemonModule {}
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { ProductResponse } from './interfaces/product-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
   //this is dependency de axios en us project
   // axios only can be used once in a project and your value dont change then the asingment inicial is the best way to use it
   private readonly axios: AxiosInstance = axios;
+
+  constructor(
+    @InjectModel(Pokemon.name) // this injectModel was made by nest team to inject model in this service
+    private readonly x: Model<Pokemon>,
+  ) {}
 
   // constructor() {
   //   // this could be replaced by a provider that allows to use axios throughout th project
