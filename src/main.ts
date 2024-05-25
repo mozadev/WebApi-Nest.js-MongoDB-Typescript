af052e1837f1cf19ac248ea250628b598bd07902
ceosmore <ceosmore@gmail.com>
2024-05-25 16:00:33 -0500

    Add transform = true and transformOption = enableImplicitConversion: true in ValidationPipe in main.ts to convert entry of string of controller in number to paginationDto

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,15 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true, // remove the properties that are not in the Dto
       forbidNonWhitelisted: true, // lanza bad request si hay propiedades que no estan en el Dto
+      transform: true, // transform the data to the Dto type
+      transformOptions: {
+        enableImplicitConversion: true, // convert the data to the Dto type
+      },
+      // convert the data to the Dto type
     }),
-  ); // validate Dtos, This pipe is used to validate the data that comes from the body of the request and the parameters of the request, apply the validation to all the methods in the controller
+  ); // validate Dtos, This pipe is used to validate the data that comes from the body
+  // of the request and the parameters of the request,
+  // apply the validation to all the methods in the controller
 
   await app.listen(3000);
 }
