9fea22f706927e5640c44bd5e53ccad349fee633
ceosmore <ceosmore@gmail.com>
2024-05-25 10:40:23 -0500

    feat: Add query parameters to findAll() method in PokemonController

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -8,11 +8,13 @@ import {
   Delete,
   HttpCode,
   HttpStatus,
+  Query,
 } from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { ParseMongoIdPipe } from 'src/common/pipes/parse-mongo-id/parse-mongo-id.pipe';
+import { query } from 'express';
 
 @Controller('pokemon')
 export class PokemonController {
@@ -25,7 +27,8 @@ export class PokemonController {
   }
 
   @Get()
-  findAll() {
+  findAll(@Query() queryParameters) {
+    console.log({ queryParameters });
     return this.pokemonService.findAll();
   }
 
