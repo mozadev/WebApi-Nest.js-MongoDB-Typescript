953cabaf22ea04c234ccbef212ea52c6a1638d77
ceosmore <ceosmore@gmail.com>
2024-05-25 19:58:07 -0500

    feat: Add @nestjs/config dependency and import ConfigModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { join } from 'path'; // en node , usually goin at start of file
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { Mongoose } from 'mongoose';
-import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 
@@ -11,6 +12,8 @@ import { SeedModule } from './seed/seed.module';
 // module always goint inside of import array
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
