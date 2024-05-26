918960368d22f033bcb5f54c2441fa3c6061c678
ceosmore <ceosmore@gmail.com>
2024-05-26 16:54:41 -0500

    feat: Update EnvConfiguration to include environment variables

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,4 +1,10 @@
-export const EnvConfiguration = () => ({});
+// This is simple function that runs smoothly in the aplicattion and get the environment variables
+export const EnvConfiguration = () => ({
+  environment: process.env.NODE_ENV || 'dev',
+  mongodb: process.env.MONGODB,
+  port: process.env.PORT || 3002,
+  default_limit: process.env.DEFAULT_LIMIT || 7,
+});
 
 // const enfv = () => {
 //     return {
